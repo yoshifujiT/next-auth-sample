172616b25c8eca04ccbbff3d77b909af2ffa9554
yoshifuji <t.yoshifuji@me.com>
2021-06-05 10:30:34 +0900

    Add session secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import Providers from 'next-auth/providers'
 
 const isProduction = process.env.NODE_ENV === 'production'
 const cookieDomain = process.env.COOKIE_DOMAIN
+const sessionSecret = process.env.SESSION_SECRET
 
 async function getUser(credentials: {
   userId: string
@@ -65,6 +66,7 @@ export default NextAuth({
       }
     },
   },
+  secret: sessionSecret,
   cookies: {
     sessionToken: {
       name: `__Secure-next-auth.session-token`,
